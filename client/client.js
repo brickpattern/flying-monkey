5b4db8ec16bc48dfee6c045a74edbbb07e620e95
Alex Soncodi <asoncodi@gmail.com>
2015-02-10 05:23:48 -0600

    Load a world.

--- client/client.js
+++ client/client.js
@@ -5,9 +5,42 @@ var app = angular.module('app', [
 ]);
 
 app.controller('Index', function($scope, $timeout) {
-  $scope.msg = 'hello from Angular!';
+  $scope.map = {
+    center: [0, 0],
+    zoom: 3,
+    options: {
+      maxZoom: 3,
+      minZoom: 3,
+      disableDefaultUI: true,
+      styles: [{
+        featureType: 'poi',
+        elementType: 'labels',
+        stylers: [{ visibility: 'off' }]
+      }]
+    },
+    events: {
+      click: function(map, event, args) {
+        var pos = args[0].latLng;
 
-  $timeout(function() {
-    $scope.msg = 'a few seconds later';
-  }, 3000);
+        console.log(pos.lat(), pos.lng());
+      }
+    }
+  };
+
+  function toCoords(pos) {
+    return {
+      longitude: pos.lng,
+      latitude: pos.lat,
+    };
+  }
+
+  function findById(collection, id) {
+    for (var i = 0; i < collection.length; i++) {
+      if (collection[i]._id == id) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
 });
